e3f623d30639fc0ff53d0a75cc798c1afb6328c2
Danh Nguyen <nguyendanhtravel@gmail.com>
2022-11-01 16:28:56 +0700

    add unit test

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/jackc/pgtype v1.12.0
 	github.com/jackc/pgx/v4 v4.12.1-0.20210724153913-640aa07df17c
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/stretchr/testify v1.8.0
 	go.uber.org/multierr v1.6.0
 	go.uber.org/zap v1.17.0
 	gopkg.in/yaml.v2 v2.4.0
@@ -17,6 +18,7 @@ require (
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -24,7 +26,10 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/puddle v1.1.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/services/wager_test.go
+++ internal/services/wager_test.go
@@ -2,57 +2,345 @@ package services
 
 import (
 	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/url"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/wager-api/internal/domains"
 	"github.com/wager-api/internal/entities"
+	"github.com/wager-api/libs/database"
+	mock_database "github.com/wager-api/mocks/libs/database"
+	mock_repositories "github.com/wager-api/mocks/repositories"
 
 	"github.com/jackc/pgtype"
 )
 
-func Test_Decode(t *testing.T) {
-	buf := new(bytes.Buffer)
-	wager := &entities.Wager{
-		WagerID: pgtype.Int4{Int: 1, Status: pgtype.Present},
-		// TotalWagerValue:     20.5,
-		// Odds:                40,
-		// SellingPercentage:   15.5,
-		// CurrentSellingPrice: 16.5,
-		// PercentageSold:      23.44,
-		// AmountSold:          10,
-		// PlacedAt: nil,
-	}
-	json.NewEncoder(buf).Encode(convertWagerPg2Domain(wager))
-	fmt.Println(buf)
+type TestCase struct {
+	ctx            context.Context
+	name           string
+	jsonReq        []byte
+	url            string
+	expectedResp   interface{}
+	expectedStatus int
+
+	setup func(ctx context.Context)
 }
 
-func Test_RoundNumber(t *testing.T) {
-	var number float32 = 12.2345
-	newnumber := roundFloat(number)
-	fmt.Println(newnumber)
+func Test_validatePlaceWagerReq(t *testing.T) {
+	t.Parallel()
+	type testcase struct {
+		name          string
+		placeWagerReq *domains.PlaceWagerRequest
+		expectedErr   error
+	}
+	tests := []testcase{
+		{
+			name:        "total_wager_value = 0",
+			expectedErr: fmt.Errorf("the total_wager_value must be a positive integer above 0"),
+			placeWagerReq: &domains.PlaceWagerRequest{
+				TotalWagerValue: 0,
+			},
+		},
+		{
+			name:        "odds = 0",
+			expectedErr: fmt.Errorf("the odds must be a positive integer above 0"),
+			placeWagerReq: &domains.PlaceWagerRequest{
+				TotalWagerValue: 10,
+				Odds:            0,
+			},
+		},
+		{
+			name:        "selling_percentage out of range [1, 100]",
+			expectedErr: fmt.Errorf("the selling_percentage must be specified as an integer between 1 and 100"),
+			placeWagerReq: &domains.PlaceWagerRequest{
+				TotalWagerValue:   10,
+				Odds:              20,
+				SellingPercentage: 110,
+			},
+		},
+		{
+			name:        "selling_price have more than 2 decimal places out of range [1, 100]",
+			expectedErr: fmt.Errorf("the selling_price must be a positive decimal value to two decimal places"),
+			placeWagerReq: &domains.PlaceWagerRequest{
+				TotalWagerValue:   10,
+				Odds:              20,
+				SellingPercentage: 50,
+				SellingPrice:      10.112,
+			},
+		},
+		{
+			name:        "selling_price lesser than total_wager_value * (selling_percentage / 100)",
+			expectedErr: fmt.Errorf("selling_price must be greater than total_wager_value * (selling_percentage / 100)"),
+			placeWagerReq: &domains.PlaceWagerRequest{
+				TotalWagerValue:   10,
+				Odds:              20,
+				SellingPercentage: 50,
+				SellingPrice:      4,
+			},
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := validatePlaceWagerReq(tc.placeWagerReq)
+			assert.Equal(t, tc.expectedErr, err)
 
-}
-
-func Test_ValidateInputWith2DemimaPoints(t *testing.T) {
-	var number float32 = 12.2
-	newNumber := number * 100
-	// numberInStringFormat := fmt.Sprintf("%f", number)
-	// getNumberDecemaPoints := strings.Split(numberInStringFormat, ".")
-	// fmt.Println(len(getNumberDecemaPoints[1]))
-	// steps := number / step
-	if newNumber-float32(int(newNumber)) > 0 {
-		fmt.Println("wrong")
+		})
 	}
 }
 
-func Test_Query(t *testing.T) {
-	var r http.Request
-	r.URL = &url.URL{
-		Scheme: "localhost:8080/wagers?page=:1&limit=:12",
+func Test_validateBuyWagerReq(t *testing.T) {
+	// t.Parallel()
+	type testcase struct {
+		name        string
+		buyWagerReq *domains.BuyWagerRequest
+		expectedErr error
 	}
-	fmt.Println(r.URL.Query().Get("page"))
+	tests := []testcase{
+		{
+			name:        "buying_price less or equal to 0",
+			expectedErr: fmt.Errorf("the buying_price must be a positive decimal"),
+			buyWagerReq: &domains.BuyWagerRequest{
+				BuyingPrice: 0,
+			},
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := validateBuyWagerReq(tc.buyWagerReq)
+			fmt.Println("===err", err)
+			assert.Equal(t, tc.expectedErr, err)
 
+		})
+	}
+}
+func Test_validateListWagerParams(t *testing.T) {
+	type testcase struct {
+		name        string
+		req         *http.Request
+		expectedErr error
+	}
+	// reqWithZeroValue
+	ctx := context.Background()
+	reqWithZeroValue := &http.Request{}
+	ctx = context.WithValue(ctx, "limit", 0)
+	ctx = context.WithValue(ctx, "page", 0)
+	reqWithZeroValue = reqWithZeroValue.WithContext(ctx)
+	//reqWithWrongFormatParam
+	ctxreqWithWrongFormatParam := context.Background()
+	reqWithWrongFormatParam := &http.Request{}
+	ctxreqWithWrongFormatParam = context.WithValue(ctxreqWithWrongFormatParam, "limit", "")
+	ctxreqWithWrongFormatParam = context.WithValue(ctxreqWithWrongFormatParam, "page", "")
+	reqWithWrongFormatParam = reqWithZeroValue.WithContext(ctxreqWithWrongFormatParam)
+
+	tests := []testcase{
+		{
+			name:        "limit = 0, page = 0",
+			expectedErr: fmt.Errorf("`page` must be positive number and `limit` should be greater than 0"),
+			req:         reqWithZeroValue,
+		},
+		{
+			name:        "req with wrong format param",
+			expectedErr: fmt.Errorf("page wrong format"),
+			req:         reqWithWrongFormatParam,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := validateListWagerParam(tc.req)
+			assert.Equal(t, tc.expectedErr, err)
+
+		})
+	}
+}
+
+func Test_PlaceWager(t *testing.T) {
+	t.Parallel()
+
+	db := &mock_database.Ext{}
+	wagerRepo := &mock_repositories.MockWagerRepo{}
+	purchaseRepo := &mock_repositories.MockPurchaseRepo{}
+	mockErr := fmt.Errorf("mock-error")
+	testcases := []TestCase{
+		{
+			// validation request
+			name:           "bad request (violate input condition)",
+			expectedResp:   []byte(`{"error":"the total_wager_value must be a positive integer above 0"}`),
+			url:            "/wagers",
+			jsonReq:        []byte(`{"total_wager_value": 0, "odds": 30,"selling_percentage": 30,"selling_price": 50}`),
+			expectedStatus: http.StatusBadRequest,
+			setup: func(ctx context.Context) {
+			},
+		},
+		{
+			name:           "err when create wager",
+			expectedResp:   []byte(`{"error":"unable to create wager"}`),
+			url:            "/wagers",
+			jsonReq:        []byte(`{"total_wager_value": 20, "odds": 30,"selling_percentage": 30,"selling_price": 50}`),
+			expectedStatus: http.StatusInternalServerError,
+			setup: func(ctx context.Context) {
+				wagerRepo.On("Create", ctx, db, mock.Anything, mock.Anything).Once().Return(mockErr)
+			},
+		},
+	}
+
+	wagerService := &WagerService{
+		DB:           db,
+		WagerRepo:    wagerRepo,
+		PurchaseRepo: purchaseRepo,
+	}
+	mockWagerHandler := WagerHandler{WagerService: wagerService}
+	for _, tc := range testcases {
+		t.Run(tc.url, func(t *testing.T) {
+			tc.setup(context.Background())
+			req := httptest.NewRequest(http.MethodPost, tc.url, bytes.NewBuffer([]byte(tc.jsonReq)))
+			rec := httptest.NewRecorder()
+			http.HandlerFunc(mockWagerHandler.WagerService.PlaceWager).ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedStatus, rec.Code)
+			data, err := ioutil.ReadAll(rec.Body)
+			assert.NoError(t, err)
+			// :len(data)-1 remove the `\n`
+			assert.Equal(t, tc.expectedResp, data[:len(data)-1])
+
+		})
+	}
+}
+func Test_BuyWager(t *testing.T) {
+	t.Parallel()
+	db := &mock_database.Ext{}
+	tx := &mock_database.Tx{}
+	wagerRepo := &mock_repositories.MockWagerRepo{}
+	purchaseRepo := &mock_repositories.MockPurchaseRepo{}
+	ctx := context.Background()
+	wagerID := 1
+	ctx = context.WithValue(ctx, "wager_id", wagerID)
+	mockErr := fmt.Errorf("mock-error")
+	testcases := []TestCase{
+		{
+			ctx:            ctx,
+			name:           "error when get the wager information",
+			expectedResp:   []byte(`{"error":"unable to buy wager: unable to get wager information"}`),
+			url:            "/buy/1",
+			jsonReq:        []byte(`{"buying_price": 20}`),
+			expectedStatus: http.StatusInternalServerError,
+			setup: func(ctx context.Context) {
+				db.On("Begin", ctx).Return(tx, nil)
+				tx.On("Rollback", mock.Anything).Return(nil)
+				wagerRepo.On("Get", ctx, tx, database.Int4(int32(wagerID))).Once().Return(nil, mockErr)
+			},
+		},
+		{
+			// validation request
+			name:           "bad request (violate input condition)",
+			expectedResp:   []byte(`{"error":"the buying_price must be a positive decimal"}`),
+			url:            "/wagers",
+			jsonReq:        []byte(`{"buying_price": 0}`),
+			expectedStatus: http.StatusBadRequest,
+			setup: func(ctx context.Context) {
+			},
+		},
+	}
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.url, func(t *testing.T) {
+			tc.setup(ctx)
+			wagerService := &WagerService{
+				DB:           db,
+				WagerRepo:    wagerRepo,
+				PurchaseRepo: purchaseRepo,
+			}
+			mockWagerHandler := WagerHandler{WagerService: wagerService}
+			req := httptest.NewRequest(http.MethodPost, tc.url, bytes.NewBuffer([]byte(tc.jsonReq)))
+			req = req.WithContext(ctx)
+			rec := httptest.NewRecorder()
+			http.HandlerFunc(mockWagerHandler.WagerService.BuyWager).ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedStatus, rec.Code)
+			data, err := ioutil.ReadAll(rec.Body)
+			assert.NoError(t, err)
+			// :len(data)-1 remove the `\n`
+			assert.Equal(t, tc.expectedResp, data[:len(data)-1])
+
+		})
+	}
+}
+
+func Test_ListWager(t *testing.T) {
+	t.Parallel()
+
+	db := &mock_database.Ext{}
+	wagerRepo := &mock_repositories.MockWagerRepo{}
+	purchaseRepo := &mock_repositories.MockPurchaseRepo{}
+	ctx := context.Background()
+	page := 1
+	limit := 10
+	nilWagerID := pgtype.Int4{}
+	_ = nilWagerID.Set(nil)
+	ctx = context.WithValue(ctx, "page", page)
+	ctx = context.WithValue(ctx, "limit", limit)
+	wagers := []*entities.Wager{
+		{
+			WagerID:         database.Int4(1),
+			TotalWagerValue: database.Float4(100),
+		},
+		{
+			WagerID:         database.Int4(2),
+			TotalWagerValue: database.Float4(100),
+		},
+	}
+	mockErr := fmt.Errorf("mock-error")
+	testcases := []TestCase{
+		{
+			ctx:          ctx,
+			name:         "happy case",
+			expectedResp: []byte(`[{"id":1,"total_wager_value":100},{"id":2,"total_wager_value":100}]`),
+			// work in both cases /wagers?page=:4&limit=:4 and /wagers?page=4&limit=4
+			url:            "/wagers?page=:4&limit=:4",
+			expectedStatus: http.StatusOK,
+			setup: func(ctx context.Context) {
+				wagerRepo.On("List", ctx, db, nilWagerID, uint32(limit)).Once().Return(wagers, nil)
+			},
+		},
+		{
+			ctx:          ctx,
+			name:         "error when list the wagers",
+			expectedResp: []byte(`{"error":"unable to list wager"}`),
+			// work in both cases /wagers?page=:4&limit=:4 and /wagers?page=4&limit=4
+			url:            "/wagers?page=:4&limit=:4",
+			expectedStatus: http.StatusInternalServerError,
+			setup: func(ctx context.Context) {
+				wagerRepo.On("List", ctx, db, nilWagerID, uint32(limit)).Once().Return(nil, mockErr)
+			},
+		},
+	}
+	wagerService := &WagerService{
+		DB:           db,
+		WagerRepo:    wagerRepo,
+		PurchaseRepo: purchaseRepo,
+	}
+	mockWagerHandler := WagerHandler{WagerService: wagerService}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.url, func(t *testing.T) {
+			tc.setup(ctx)
+
+			req := httptest.NewRequest(http.MethodGet, tc.url, bytes.NewBuffer([]byte(tc.jsonReq)))
+			req = req.WithContext(ctx)
+			rec := httptest.NewRecorder()
+			http.HandlerFunc(mockWagerHandler.WagerService.ListWager).ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedStatus, rec.Code)
+			data, err := ioutil.ReadAll(rec.Body)
+			assert.NoError(t, err)
+			// :len(data)-1 remove the `\n`
+			assert.Equal(t, tc.expectedResp, data[:len(data)-1])
+
+		})
+	}
 }
